9a1f45249f9766bff908dbc88804df353f800e8f
aliwar <alix.warnke@assaabloy.com>
2020-11-23 23:56:43 +0100

    Added more stocks

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/home/StockPriceRetriever.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/home/StockPriceRetriever.kt
@@ -15,7 +15,7 @@ class StockPriceRetriever(private val stockPriceProducer: StockPriceProducer, pr
 
     companion object {
 
-        val SYMBOLS = arrayOf("MIPS.ST", "ASSA-B.ST", "AZELIO.ST")
+        val SYMBOLS = arrayOf("MIPS.ST", "ASSA-B.ST", "AZELIO.ST", "SAS.ST", "ANOT.ST", "IPCO.ST")
 
     }
 
