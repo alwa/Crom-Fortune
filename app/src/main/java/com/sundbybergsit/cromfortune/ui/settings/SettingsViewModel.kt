f32c65ed2046c529a135e25b70488814d3b6f5e9
aliwar <alix.warnke@assaabloy.com>
2021-02-04 13:49:25 +0100

    Updated todo list

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsViewModel.kt
@@ -12,7 +12,13 @@ class SettingsViewModel : ViewModel() {
     val text: LiveData<String> = _text
 
     private val _todoText = MutableLiveData<String>().apply {
-        value = "Att göra: 1. Rekursion på Croms rekommendationer, 2. Jämför utfall med Crom"
+        value = "Att göra: 1. Fixa rekommendationsbugg (vill att man köper till högre kurs) " +
+                "2. Home: Lägg till vinst-kolumn " +
+                "3. Rekursion på Croms rekommendationer, " +
+                "4. Fixa så notifieringar skapas utan att man går in i dashboard " +
+                "5. Home: Lägg till sorteringsmöjligheter (Bäst idag, sämst idag, alfabetisk sortering) " +
+                "6. Dashboard: Lägg till Croms skugghandel " +
+                "7. Home: Implementera köp/sälj-knappar"
     }
     val todoText: LiveData<String> = _todoText
 
