2c998d85b92f405bf147c23d8f5e01384eaebe95
aliwar <alix.warnke@assaabloy.com>
2021-02-06 23:03:31 +0100

    Down-prioritization of Crom recursion

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsViewModel.kt
@@ -12,11 +12,12 @@ class SettingsViewModel : ViewModel() {
     val text: LiveData<String> = _text
 
     private val _todoText = MutableLiveData<String>().apply {
-        value = "Att göra: 1. Rekursion på Croms rekommendationer, " +
-                "2. Fixa så notifieringar skapas utan att man går in i dashboard " +
-                "3. Fixa refresh-bugg för när man har tagit bort en transaktion (byt tabb och tillbaka så länge) " +
-                "4. Förbättra beräkning av GAV att inte ta hänsyn till gamla irrelevanta transaktioner " +
-                "5. Home: Lägg till sorteringsmöjligheter (Bäst idag, sämst idag, alfabetisk sortering) " +
+        value = "Att göra: " +
+                "1. Fixa så notifieringar skapas utan att man går in i dashboard " +
+                "2. Fixa refresh-bugg för när man har tagit bort en transaktion (byt tabb och tillbaka så länge) " +
+                "3. Förbättra beräkning av GAV att inte ta hänsyn till gamla irrelevanta transaktioner " +
+                "4. Home: Lägg till sorteringsmöjligheter (Bäst idag, sämst idag, alfabetisk sortering) " +
+                "5. Rekursion på Croms rekommendationer, "
                 "6. Dashboard: Lägg till Croms skugghandel " +
                 "7. Home: Implementera köp/sälj-knappar" +
                 "8. Dark theme vid vissa klockslag"
