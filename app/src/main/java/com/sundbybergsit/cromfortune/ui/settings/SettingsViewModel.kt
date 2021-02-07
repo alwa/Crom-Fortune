b6ce2bdeed17931f2e2064e2ce50100da396b5f2
aliwar <alix.warnke@assaabloy.com>
2021-02-07 23:53:35 +0100

    Fixed TODO

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsViewModel.kt
@@ -13,15 +13,15 @@ class SettingsViewModel : ViewModel() {
 
     private val _todoText = MutableLiveData<String>().apply {
         value = "Att göra: " +
-                "1. Fixa refresh-bugg för när man har tagit bort en transaktion (byt tabb och tillbaka så länge), " +
+                "1. Fyll i valutafältet automatiskt vid val av aktier, " +
                 "2. Förbättra beräkning av GAV att inte ta hänsyn till gamla irrelevanta transaktioner, " +
                 "3. Home: Lägg till sorteringsmöjligheter (Bäst idag, sämst idag, alfabetisk sortering), " +
-                "4. Snygga till Croms 'compliance score', " +
-                "5. Rekursion på Croms rekommendationer, "
-                "6. Dashboard: Lägg till Croms skugghandel, " +
-                "7. Home: Implementera köp/sälj-knappar, " +
-                "8. Dark theme vid vissa klockslag, " +
-                "9. Home: Total vinst vid vissa klockslag, "
+                "4. Home: Implementera köp/sälj-knappar, " +
+                "5. Dashboard: Lägg till Croms skugghandel, " +
+                "6. Snygga till Croms 'compliance score', " +
+                "7. Dark theme vid vissa klockslag, " +
+                "8. Fixa refresh-bugg för när man har tagit bort en transaktion (byt tabb och tillbaka så länge), " +
+                "9. Home: Total vinst"
     }
     val todoText: LiveData<String> = _todoText
 
