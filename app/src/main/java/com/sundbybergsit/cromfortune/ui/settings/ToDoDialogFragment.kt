e61d31dbbf0b7ebcf57b195dfbe9f4e4ac467ffb
aliwar <alix.warnke@assaabloy.com>
2021-02-22 09:35:30 +0100

    Updated TODO

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/ToDoDialogFragment.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/ToDoDialogFragment.kt
@@ -16,14 +16,17 @@ class ToDoDialogFragment : DialogFragment() {
                 "2. Inställningar: Ställ in tidsintervall då kurser ska hämtas\n" +
                 "3. Notifieringar: Lägg till arkiv\n" +
                 "4. Notiferingar: Lägg till humpnotiser då en kurs är lägre än senaste sälj (vid 0 aktier)\n" +
-                "5. Lägg till anteckningar per aktie\n"
+                "5. Lägg till anteckningar per aktie\n" +
                 "6. Dark theme vid vissa klockslag\n" +
                 "7. Hem: Implementera köp/sälj-knappar\n" +
                 "8. Hem: Sortering (bäst/sämst/ABC)\n" +
                 "9. Inställningar: Lägg till alarm\n" +
                 "10. Hem: Fixa refresh-bugg för när man har tagit bort en transaktion (byt tabb och tillbaka så länge)\n" +
-                "11. Inställningar: Implementera stöd för individuell courtageplan\n" +
-                "12. Instrumentbräda: Exkludera transaktioner från 0 -> x aktier från Crom conformance score\n"
+                "11. Hem: Lägg till stöd för utdelningar\n" +
+                "12. Hem: Lägg till stöd för nyemissioner\n" +
+                "13. Hem: Lägg till stöd för omvända splitar\n" +
+                "14. Inställningar: Implementera stöd för individuell courtageplan\n" +
+                "15. Instrumentbräda: Exkludera transaktioner från 0 -> x aktier från Crom conformance score\n"
         val context = requireContext()
         return AlertDialog.Builder(context)
                 .setMessage(message)
