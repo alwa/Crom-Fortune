d417337f2deca02bb2630a545c4ff1bd5c5bcda7
aliwar <alix.warnke@assaabloy.com>
2021-03-06 15:40:16 +0100

    Refactoring: Cleanup of dead code

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/dashboard/DashboardFragment.kt
@@ -2,7 +2,6 @@ package com.sundbybergsit.cromfortune.ui.dashboard
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.sundbybergsit.cromfortune.R
@@ -13,11 +12,8 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
     private val dashboardViewModel: DashboardViewModel by activityViewModels()
 
-    private lateinit var infoText: TextView
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        infoText = view.findViewById(R.id.textView_fragmentDashboard)
         setupDataListeners()
     }
 
